bae8d232577f925c51f60b53c6d2ee0d9bc72790
rjk-info <therjkinfo@gmail.com>
2025-02-09 17:14:32 +0530

    Add files via upload

---
+++ MyProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a , b;
+            Console.WriteLine("Enter the first number");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum of the two numbers is: " + (a + b));
+            Console.ReadKey();
+        }
+    }
+}
